c2065e24054559b057a03633dc6ecec36827fb58
81670 <jiwook@81670.local>
2023-03-27 23:13:08 +0900

    remove dist folder

--- dist/routes/itemPrice.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const lostarkPrice_1 = require("@src/controllers/lostarkPrice");
-const router = (0, express_1.Router)();
-router.get("/lostArkMarketItem", lostarkPrice_1.setLostArkMarketItemPrice);
-exports.default = router;
