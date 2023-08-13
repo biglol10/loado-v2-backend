6019922994cdfbfa646b0ee5f81637d3c4016287
81670 <jiwook@81670.local>
2023-08-13 17:00:49 +0900

    changed redis connection

--- src/utils/redisInstance.ts
+++ src/utils/redisInstance.ts
@@ -5,7 +5,9 @@ const redisClientUrl =
     ? "127.0.0.1:6379"
     : process.env.REDIS_ENDPOINT;
 
-const redisInstance = createClient();
+const redisInstance = createClient({
+  url: redisClientUrl,
+});
 
 redisInstance.on("error", (err) => console.log("Redis Client Error", err));
 redisInstance.connect();
