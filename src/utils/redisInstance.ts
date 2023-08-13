7c70446372dcd9735ae53fba1f0a37a5ae5daac0
81670 <jiwook@81670.local>
2023-08-13 17:50:32 +0900

    payment 안 드는 걸로 교체

--- src/utils/redisInstance.ts
+++ src/utils/redisInstance.ts
@@ -1,5 +1,8 @@
 import { createClient } from "redis";
 
+// Heroku data for redis 일 경우 process.env.REDIS_URL
+// Redis Enterprise Cloud 일 경우 process.env.REDISCLOUD_URL (밑에 Security에 Username, pw 확인 가능)
+
 const redisClientUrl =
   process.env.NODE_ENV === "dev"
     ? "127.0.0.1:6379"
