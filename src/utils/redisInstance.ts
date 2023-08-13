b24dad7f6161fc0f34a61c276bceeeb523230648
81670 <jiwook@81670.local>
2023-08-13 16:46:32 +0900

    changed createClient

--- src/utils/redisInstance.ts
+++ src/utils/redisInstance.ts
@@ -4,10 +4,7 @@ const redisInstance =
   process.env.npm_lifecycle_event === "dev"
     ? createClient()
     : createClient({
-        password: process.env.REDIS_PW,
-        socket: {
-          host: process.env.REDIS_ENDPOINT,
-        },
+        url: process.env.REDIS_ENDPOINT,
       });
 
 redisInstance.on("error", (err) => console.log("Redis Client Error", err));
