39231534b5282fea35702024bd3e72bd2b8ba181
biglol <biglol@Jiwookui-noteubug.local>
2023-11-18 18:27:56 +0900

    changed redis cache time

--- src/controllers/lostarkPrice.ts
+++ src/controllers/lostarkPrice.ts
@@ -7,32 +7,28 @@ import MarketItemStatsModel from "../models/MarketItemStats";
 import { extractNumber } from "../utils/customFunc";
 import CustomError from "../utils/CustomError";
 
-export const getLostArkMarketItemPrice = asyncHandler(
-  async (req, res, next) => {}
-);
+export const getLostArkMarketItemPrice = asyncHandler(async (req, res, next) => {});
 
-export const getCurrentMarketItemPrice = asyncHandler(
-  async (req, res, next) => {
-    const itemList = req.body.itemList as string[];
+export const getCurrentMarketItemPrice = asyncHandler(async (req, res, next) => {
+  const itemList = req.body.itemList as string[];
 
-    const resultArr = [];
+  const resultArr = [];
 
-    for (let itemName of itemList) {
-      const itemPriceResult = await MarketItemModel.findOne({
-        itemName,
-      })
-        .select("-_id itemId itemName currentMinPrice")
-        .sort({ createdAt: -1 });
+  for (let itemName of itemList) {
+    const itemPriceResult = await MarketItemModel.findOne({
+      itemName,
+    })
+      .select("-_id itemId itemName currentMinPrice")
+      .sort({ createdAt: -1 });
 
-      resultArr.push(itemPriceResult);
-    }
-
-    return res.status(200).json({
-      result: "success",
-      resultArr,
-    });
+    resultArr.push(itemPriceResult);
   }
-);
+
+  return res.status(200).json({
+    result: "success",
+    resultArr,
+  });
+});
 
 export const getPeriodMarketItemPrice = asyncHandler(async (req, res, next) => {
   const { itemName, startDate, endDate } = req.query;
@@ -51,37 +47,35 @@ export const getPeriodMarketItemPrice = asyncHandler(async (req, res, next) => {
   });
 });
 
-export const getPeriodYearMonthMarketItemPrice = asyncHandler(
-  async (req, res, next) => {
-    const { itemId, year, month, startDate, endDate } = req.query;
+export const getPeriodYearMonthMarketItemPrice = asyncHandler(async (req, res, next) => {
+  const { itemId, year, month, startDate, endDate } = req.query;
 
-    let parsedStartDate: Date | undefined;
-    let parsedEndDate: Date | undefined;
+  let parsedStartDate: Date | undefined;
+  let parsedEndDate: Date | undefined;
 
-    // Check if year and month values are provided
-    if (year && month) {
-      const parsedYear = parseInt(year as string);
-      const parsedMonth = parseInt(month as string);
-      if (!isNaN(parsedYear) && !isNaN(parsedMonth)) {
-        parsedStartDate = new Date(parsedYear, parsedMonth - 1, 1);
-        parsedEndDate = new Date(parsedYear, parsedMonth, 0, 23, 59, 59, 999);
-      }
+  // Check if year and month values are provided
+  if (year && month) {
+    const parsedYear = parseInt(year as string);
+    const parsedMonth = parseInt(month as string);
+    if (!isNaN(parsedYear) && !isNaN(parsedMonth)) {
+      parsedStartDate = new Date(parsedYear, parsedMonth - 1, 1);
+      parsedEndDate = new Date(parsedYear, parsedMonth, 0, 23, 59, 59, 999);
     }
-
-    const data = await MarketItemStatsModel.find({
-      itemId: itemId as string,
-      date: {
-        $gte: dayjs(parsedStartDate).format("YYYY-MM-DD") || startDate,
-        $lte: dayjs(parsedEndDate).format("YYYY-MM-DD") || endDate,
-      },
-    });
-
-    return res.status(200).json({
-      result: "success",
-      data,
-    });
   }
-);
+
+  const data = await MarketItemStatsModel.find({
+    itemId: itemId as string,
+    date: {
+      $gte: dayjs(parsedStartDate).format("YYYY-MM-DD") || startDate,
+      $lte: dayjs(parsedEndDate).format("YYYY-MM-DD") || endDate,
+    },
+  });
+
+  return res.status(200).json({
+    result: "success",
+    data,
+  });
+});
 
 // categoryCode mapping
 export const testtest = asyncHandler(async (req, res, next) => {
@@ -149,147 +143,139 @@ export const testtest = asyncHandler(async (req, res, next) => {
   });
 });
 
-export const getCurrentAuctionItemPrice = asyncHandler(
-  async (req, res, next) => {
-    const startDate = "2023-04-01";
-    const endDate = dayjs("2023-04-28").add(1, "day").format("YYYY-MM-DD");
+export const getCurrentAuctionItemPrice = asyncHandler(async (req, res, next) => {
+  const startDate = "2023-04-01";
+  const endDate = dayjs("2023-04-28").add(1, "day").format("YYYY-MM-DD");
 
-    const model = await AuctionItemModel.aggregate([
-      {
-        $addFields: {
-          date: {
-            $dateFromString: {
-              dateString: "$createdAt",
-            },
+  const model = await AuctionItemModel.aggregate([
+    {
+      $addFields: {
+        date: {
+          $dateFromString: {
+            dateString: "$createdAt",
           },
         },
       },
-      {
-        $match: {
-          date: {
-            $gte: new Date(startDate),
-            $lte: new Date(endDate),
-          },
+    },
+    {
+      $match: {
+        date: {
+          $gte: new Date(startDate),
+          $lte: new Date(endDate),
         },
       },
-      {
-        $group: {
-          _id: {
-            day: { $dayOfMonth: "$date" },
-            month: { $month: "$date" },
-            year: { $year: "$date" },
-            itemName: "$itemName",
-          },
-          avgBuyPrice: { $avg: "$auctionInfo.BuyPrice" },
-          minBuyPrice: { $min: "$auctionInfo.BuyPrice" },
-          maxBuyPrice: { $max: "$auctionInfo.BuyPrice" },
+    },
+    {
+      $group: {
+        _id: {
+          day: { $dayOfMonth: "$date" },
+          month: { $month: "$date" },
+          year: { $year: "$date" },
+          itemName: "$itemName",
         },
+        avgBuyPrice: { $avg: "$auctionInfo.BuyPrice" },
+        minBuyPrice: { $min: "$auctionInfo.BuyPrice" },
+        maxBuyPrice: { $max: "$auctionInfo.BuyPrice" },
       },
-      {
-        $addFields: {
-          formattedDate: {
-            $dateToString: {
-              format: "%Y-%m-%d",
-              date: {
-                $dateFromParts: {
-                  year: "$_id.year",
-                  month: "$_id.month",
-                  day: "$_id.day",
-                },
+    },
+    {
+      $addFields: {
+        formattedDate: {
+          $dateToString: {
+            format: "%Y-%m-%d",
+            date: {
+              $dateFromParts: {
+                year: "$_id.year",
+                month: "$_id.month",
+                day: "$_id.day",
               },
             },
           },
         },
       },
-      {
-        $project: {
-          _id: 0,
-          itemName: "$_id.itemName",
-          formattedDate: 1,
-          avgBuyPrice: 1,
-          minBuyPrice: 1,
-          maxBuyPrice: 1,
-        },
+    },
+    {
+      $project: {
+        _id: 0,
+        itemName: "$_id.itemName",
+        formattedDate: 1,
+        avgBuyPrice: 1,
+        minBuyPrice: 1,
+        maxBuyPrice: 1,
       },
-      {
-        $sort: { formattedDate: 1, itemName: 1 },
-      },
-    ])
-      .then((result) => {
-        return res.status(200).json({
-          result: "success",
-          data: result,
-          day: dayjs().tz("Asia/Seoul").subtract(1, "day").format("YYYY-MM-DD"),
-        });
-      })
-      .catch((err) => {
-        return res.status(400).json({
-          result: "success",
-          err,
-        });
+    },
+    {
+      $sort: { formattedDate: 1, itemName: 1 },
+    },
+  ])
+    .then((result) => {
+      return res.status(200).json({
+        result: "success",
+        data: result,
+        day: dayjs().tz("Asia/Seoul").subtract(1, "day").format("YYYY-MM-DD"),
       });
-  }
-);
-
-export const setLostArkMarketItemPrice = asyncHandler(
-  async (req, res, next) => {
-    const itemRes = await BaseService.request({
-      method: "post",
-      url: "/markets/items",
-      data: {
-        Sort: "CURRENT_MIN_PRICE",
-        CategoryCode: 50000,
-        ItemTier: 0,
-        ItemName: "정제된 파괴강석",
-        PageNo: 1,
-        SortCondition: "DESC",
-      },
+    })
+    .catch((err) => {
+      return res.status(400).json({
+        result: "success",
+        err,
+      });
     });
+});
 
-    if (itemRes.hasOwnProperty("Items")) {
-      try {
-        const exactItemFilter = itemRes["Items"].filter(
-          (item: any) => item.Name === "정제된 파괴강석"
-        );
+export const setLostArkMarketItemPrice = asyncHandler(async (req, res, next) => {
+  const itemRes = await BaseService.request({
+    method: "post",
+    url: "/markets/items",
+    data: {
+      Sort: "CURRENT_MIN_PRICE",
+      CategoryCode: 50000,
+      ItemTier: 0,
+      ItemName: "정제된 파괴강석",
+      PageNo: 1,
+      SortCondition: "DESC",
+    },
+  });
 
-        const exactItem = exactItemFilter[0];
+  if (itemRes.hasOwnProperty("Items")) {
+    try {
+      const exactItemFilter = itemRes["Items"].filter((item: any) => item.Name === "정제된 파괴강석");
 
-        const newMarketRecord = new MarketItemModel({
-          itemId: exactItem.Id,
-          itemName: exactItem.Name,
-          itemGrade: exactItem.Grade,
-          yDayAvgPrice: exactItem.YDayAvgPrice,
-          recentPrice: exactItem.RecentPrice,
-          currentMinPrice: exactItem.CurrentMinPrice,
+      const exactItem = exactItemFilter[0];
+
+      const newMarketRecord = new MarketItemModel({
+        itemId: exactItem.Id,
+        itemName: exactItem.Name,
+        itemGrade: exactItem.Grade,
+        yDayAvgPrice: exactItem.YDayAvgPrice,
+        recentPrice: exactItem.RecentPrice,
+        currentMinPrice: exactItem.CurrentMinPrice,
+      });
+
+      newMarketRecord
+        .save()
+        .then(() => console.log("Market Record created successfully"))
+        .catch((err) => {
+          console.log("Error creating Market Record:", err);
+          throw new Error("Error when saving Market Record to MongoDB");
         });
 
-        newMarketRecord
-          .save()
-          .then(() => console.log("Market Record created successfully"))
-          .catch((err) => {
-            console.log("Error creating Market Record:", err);
-            throw new Error("Error when saving Market Record to MongoDB");
-          });
-
-        return res.status(200).json({
-          result: "success",
-          data: itemRes,
-        });
-      } catch {
-        throw new Error("Item not found");
-      }
+      return res.status(200).json({
+        result: "success",
+        data: itemRes,
+      });
+    } catch {
+      throw new Error("Item not found");
     }
-
-    throw new Error("Item not found");
   }
-);
+
+  throw new Error("Item not found");
+});
 
 export const getMarketPriceByCategoryCode = asyncHandler(async (req, res) => {
   const { categoryCode, timeValue } = req.query;
 
-  const latestStats = await MarketItemStatsModel.findOne()
-    .sort({ date: -1 })
-    .exec();
+  const latestStats = await MarketItemStatsModel.findOne().sort({ date: -1 }).exec();
 
   const latestDate = latestStats?.date;
 
--- src/middleware/redisMiddleware.ts
+++ src/middleware/redisMiddleware.ts
@@ -2,11 +2,7 @@ import { Request, Response, NextFunction } from "express";
 import redisInstance from "../utils/redisInstance";
 
 // 600ms ~ 1.16s 에서 220ms ~ 420ms로 줄어든 걸 확인할 수 있음
-const redisMiddleware = async (
-  req: Request,
-  res: Response,
-  next: NextFunction
-) => {
+const redisMiddleware = async (req: Request, res: Response, next: NextFunction) => {
   const cacheKey = req.originalUrl; // 예시: /api/loadoPrice/getMarketPriceByCategoryCode?categoryCode=44420
 
   try {
@@ -15,13 +11,10 @@ const redisMiddleware = async (
     if (cachedData) {
       res.json(JSON.parse(cachedData));
     } else {
-      // Override the res.json method to intercept JSON responses
       const originalJson = res.json;
       res.json = (data) => {
-        // Cache the response data
-        redisInstance.set(cacheKey, JSON.stringify(data), { EX: 21600 }); // Set expiry in seconds (6 hours)
+        redisInstance.set(cacheKey, JSON.stringify(data), { EX: 300 }); // Set expiry in seconds (5 min)
 
-        // Call the original res.json method to send the response
         return originalJson.call(res, data); // Return the result of the original method
       };
 
